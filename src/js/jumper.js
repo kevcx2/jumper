3c270c7d9e19f39c022fbc7ee1c4d706201ef0bd
Tomas Batalla <tebatalla@gmail.com>
2015-04-28 10:58:34 -0700

    Remove hello phaser

--- src/js/jumper.js
+++ src/js/jumper.js
@@ -1,14 +1,17 @@
 window.onload = function() {
 
-    var game = new Phaser.Game(600, 400, Phaser.AUTO, '', { preload: preload, create: create });
+  var game = new Phaser.Game(600, 400, Phaser.AUTO, '', { preload: preload, create: create });
+  var platforms;
 
-    function preload () {
-        game.load.image('logo', '/img/phaser.png');
-    }
+  function preload () {
+    
+  }
 
-    function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    }
+  function create () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+  }
 
 };
