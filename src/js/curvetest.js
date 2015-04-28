525d747a21246c17c9dc59ca845d4c51769a0d8d
Kevin Cox <kev.r.cox90@gmail.com>
2015-04-28 10:57:58 -0700

    add sprites to play with

---
+++ src/js/curvetest.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    var game = new Phaser.Game(600, 400, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+    }
+
+    function create () {
+    }
+
+};
