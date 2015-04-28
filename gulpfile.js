2d4fda05df26f7eb0178d7f0fd28a4579a6c75fb
Tomas Batalla <tebatalla@gmail.com>
2015-04-27 21:19:45 -0700

    Add app files

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ var gulp = require('gulp'),
 
 var vendorStream = gulp.src(['./vendor/js/*.js'], {read: false});
 
-// var appStream = gulp.src(['./src/app/*.js'], {read: false});
+var appStream = gulp.src(['./src/js/*.js'], {read: false});
 
 gulp.src('./views/index.ejs')
-  .pipe(inject(series(vendorStream), { ignorePath: 'vendor/'})) // This will always inject vendor files before app files
+  .pipe(inject(series(vendorStream, appStream), { ignorePath: ['vendor/', 'src/']}))
   .pipe(gulp.dest('./views/'));
 
 gulp.task('sass', function () {
