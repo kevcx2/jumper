305d87f0c97994b471aa1a23fc40612b42ada5ed
Kevin Cox <kev.r.cox90@gmail.com>
2015-04-30 20:57:53 -0700

    display drawn line path for curves instead of calculated points

--- src/js/curvetest.js
+++ src/js/curvetest.js
@@ -4,7 +4,6 @@ var game = new Phaser.Game(640, 480, Phaser.AUTO, 'game');
 
 var CurveTest = function () {
 
-  this.bmd = null;
   this.startX = 100;
   this.startY = 200;
 
@@ -13,8 +12,10 @@ var CurveTest = function () {
   this.jumpHeightStart = 0;
   this.jumpReady = false;
 
-  this.HEIGHT_SENSITIVITY = 1.2;
+  this.HEIGHT_SENSITIVITY = 2;
   this.POINT_DENSITY = 100;
+
+  this.lastCurve = undefined;
 };
 
 CurveTest.prototype = {
@@ -22,69 +23,56 @@ CurveTest.prototype = {
   init: function () {
     game.renderer.renderSession.roundPixels = true;
     this.stage.backgroundColor = '#204090';
+    game.time.advancedTiming = true;
   },
 
   preload: function () {
   },
 
   create: function () {
-    this.bmd = this.add.bitmapData(game.width, game.height);
-    this.bmd.addToWorld();
+    //debug fps
+    this.fpsText = game.add.text(0, 0, '');
+
     game.input.onDown.add(this.adjustCurve, this);
     game.input.onUp.add(this.jump, this);
   },
 
   adjustCurve: function (clickEvent) {
-    this.jumpready = true;
-    this.jumpHeightStart = clickEvent.clientY;
-    console.log(this.jumpHeight);
-  },
-
-  jump: function () {
     this.jumpDistance = 0;
     this.jumpHeight = 0;
     this.jumpHeightStart = 0;
+    this.jumpready = true;
+    this.jumpHeightStart = clickEvent.clientY;
+  },
+
+  jump: function () {
     this.jumpready = false;
   },
 
-  plot: function (points) {
-    this.path = [];
-
-    var x = 1 / this.POINT_DENSITY;
-
-    for (var i = 0; i <= 1; i += x)
-    {
-      var px = this.math.bezierInterpolation(points.x, i);
-      var py = this.math.bezierInterpolation(points.y, i);
-
-      this.path.push( { x: px, y: py });
-
-      this.bmd.rect(px, py, 1, 1, 'rgba(255, 255, 255, 1)');
-    }
-
-    for (var p = 0; p < points.x.length; p++)
-    {
-        this.bmd.rect(points.x[p]-3, points.y[p]-3, 6, 6, 'rgba(255, 0, 0, 1)');
-    }
-  },
-
   update: function () {
-    this.bmd.clear();
 
     if (this.jumpready === true) {
       this.jumpDistance += 2;
       this.jumpHeight = Math.abs(this.jumpHeightStart - game.input.y) * this.HEIGHT_SENSITIVITY;
     }
-    var jumpPoints = {
-      'x': [ this.startX, this.startX + this.jumpDistance/2, this.startX + this.jumpDistance],
-      'y': [ this.startY, this.startY - this.jumpHeight, this.startY]
-    };
-    var mirrorJumpPoints = {
-      'x': [ this.startX, this.startX + this.jumpDistance/2, this.startX + this.jumpDistance],
-      'y': [ this.startY, this.startY + this.jumpHeight, this.startY]
-    };
-    this.plot(mirrorJumpPoints);
-    this.plot(jumpPoints);
+
+    if (this.lastCurve) {
+      this.lastCurve.destroy();
+      this.lastCurveMirror.destroy();
+    }
+    //potential source of slowdown - revisit if performance is an issue
+    // building / destroying graphics objects is somewhat cpu intensive
+    var jumpPath = game.add.graphics(this.startX, this.startY);
+    jumpPath.lineStyle(2, 0x99FFCC, 1);
+    jumpPath.quadraticCurveTo(this.jumpDistance/2, -this.jumpHeight, this.jumpDistance, 0);
+    this.lastCurve = jumpPath;
+
+    var jumpPathMirror = game.add.graphics(this.startX, this.startY);
+    jumpPathMirror.lineStyle(2, 0x99FFCC, 1);
+    jumpPathMirror.quadraticCurveTo(this.jumpDistance/2, +this.jumpHeight, this.jumpDistance, 0);
+    this.lastCurveMirror = jumpPathMirror;
+
+    this.fpsText.text = game.time.fps;
   }
 };
 
