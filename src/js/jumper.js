e8c704823b23dc59bb1116504df98fa7c88d4abe
Kevin Cox <kev.r.cox90@gmail.com>
2015-05-13 09:29:27 -0700

    add infinite scroll & separate out player class into new file

--- src/js/jumper.js
+++ src/js/jumper.js
@@ -1,168 +1,105 @@
 window.onload = function() {
 
-  var game = new Phaser.Game(600, 400, Phaser.AUTO, '', {
+  var game = new Phaser.Game(400, 500, Phaser.AUTO, '', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
   });
-
-  var Player = function (game, playerSprite) {
-
-    this.game = game;
-    this.jumping = false;
-    this.jumpready = false;
-
-    this.jumpDistance = 0;
-    this.jumpHeight = 0;
-    this.jumpHeightStart = 0;
-    this.jumpReady = false;
-
-    this.jumpGuideStartSpeed = 2.8;
-    this.jumpGuideSpeed = this.jumpGuideStartSpeed;
-    this.jumpGuideAccel = 1.03;
-
-    this.HEIGHT_SENSITIVITY = 2;
-    this.POINT_DENSITY = 100;
-    this.lastCurve = undefined; // to hold graphics object of last drawn curve - will be reset every frame
-    this.pointGraphics = [];
-    // this.lastCurveMirror = undefined;
-
-    Phaser.Sprite.call(this, game, game.width / 2 + 50, 300, playerSprite);
-  };
-
-  Player.prototype = Object.create(Phaser.Sprite.prototype, {
-
-    prepareToJump: {
-      value: function (clickEvent) {
-        this.jumpready = true;
-        this.jumpHeightStart = clickEvent.clientX;
-        this.game.input.onDown.remove(this.prepareToJump, this);
-        this.game.input.onUp.add(this.jump, this);
-      }
-    },
-
-    jump: {
-      value: function () {
-        console.log(this.points);
-        var tween = this.game.add.tween(this);
-        tween.to({
-          x: this.points.x,
-          y: this.points.y
-        }, 800);
-        tween.interpolation(Phaser.Math.bezierInterpolation);
-        tween.start();
-        this.jumping = true;
-        this.jumpready = false;
-        // this.body.velocity.x = 200;
-        this.jumpDistance = 0;
-        this.jumpHeight = 0;
-        this.jumpHeightStart = 0;
-        this.jumpGuideSpeed = this.jumpGuideStartingSpeed;
-        this.jumpready = false;
-        this.game.input.onUp.remove(this.jump, this);
-      }
-    },
-  });
-
-  Player.prototype.constructor = Player;
 
   var platforms;
   var circle;
+  var score;
+  var addPlats = false;
+  var topPlat = 0;
+  var platDist = -200;
+  var platDistRange = -125;
 
   function preload () {
     game.load.image('circle', 'sprites/circle.png');
     game.load.image('square', 'sprites/square.png');
     game.load.image('triangle', 'sprites/triangle.png');
+    game.camera.bounds = null;
   }
 
   function create () {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     circle = new Player(game, 'circle');
-    circle.anchor.set(0.5);
-    circle.enableBody = true;
     game.physics.arcade.enable(circle);
-    circle.body.bounce.x = 0.2;
-    // circle.body.collideWorldBounds = true;
 
     game.add.existing(circle);
-
     platforms = game.add.group();
-    platforms.enableBody = true;
 
-    var ledge = platforms.create(game.width / 2, game.height / 2, 'square');
-
-    ledge.body.immovable = true;
+    //create starting platform
+    var ledge = platforms.create(circle.x - 3, circle.y - 3, 'square');
     ledge.anchor.set(0.5, 0.5);
+    game.physics.arcade.enable(ledge);
 
-    ledge = platforms.create(game.width / 2, 300, 'square');
+    //create 20 platforms
+    for(var i = 0; i < 20; i++) {
+      topPlat = topPlat + platDist + (Math.random() * platDistRange);
+      ledge = platforms.create(game.width / 2, topPlat, 'square');
+      ledge.anchor.set(0.5, 0.5);
+      game.physics.arcade.enable(ledge);
+    }
 
-    ledge.body.immovable = true;
-    ledge.anchor.set(0.5, 0.5);
-
-    // Set up handlers for mouse events
-    game.input.onDown.add(circle.prepareToJump, circle);
-    // game.input.addMoveCallback(mouseDragMove, this);
+    score = game.add.text(0, 0, "Score: " + circle.score,
+      { font: "18px Arial", fill: "#fff", align: "left" }
+    );
   }
 
-  function checkLanding (circle, platform){
-    if(platform.x <= circle.x + circle.width / 2 ){
-      var border = circle.y - platform.y;
-      if(Math.abs(border) > 20){
-        circle.body.velocity.y = border * 2;
-        circle.body.velocity.x = -200;
+  function checkLanding (circle, platforms){
+    if (!circle.jumping) {
+      var jumpStatus = false;
+      for(var i = 0; i < platforms.length; i++) {
+        if (Phaser.Rectangle.intersects(circle.body, platforms.children[i].body)) {
+          jumpStatus = true;
+        }
       }
+      return jumpStatus;
+    }
+    return true;
+  }
 
-      if(circle.jumping){
-        circle.jumping = false;
-        game.input.onDown.add(circle.prepareToJump, circle);
-      }
+  function addPlatforms() {
+    //create 10 platforms
+    for(var i = 0; i < 10; i++) {
+      topPlat = topPlat + platDist + (Math.random() * platDistRange);
+      ledge = platforms.create(game.width / 2, topPlat, 'square');
+      ledge.anchor.set(0.5, 0.5);
+      game.physics.arcade.enable(ledge);
     }
   }
 
 
   function update () {
-
-    game.physics.arcade.collide(circle, platforms, checkLanding);
-    if (circle.jumpready === true) {
-      circle.jumpGuideSpeed *= circle.jumpGuideAccel;
-      circle.jumpDistance += circle.jumpGuideSpeed;
-      circle.jumpHeight = Math.abs(circle.jumpHeightStart - game.input.x) * circle.HEIGHT_SENSITIVITY;
+    game.camera.y = circle.y - (game.height * (3/4)) + (circle.height / 2);
+    if (!circle.jumping) {
+      console.log(checkLanding(circle, platforms));
     }
 
-    // trace jump path
-    // rect = game.add.graphics(circle.x, circle.y);
-    // rect.lineStyle(1, 0x0000FF, 1);
-    // rect.drawRect(1,1,1,1);
+    //update score
+    score.text = "Score: " + circle.score;
+    score.x = game.camera.x;
+    score.y = game.camera.y;
 
-    circle.points = {
-      'x': [ circle.x, circle.x + circle.jumpHeight + (circle.jumpHeight * 0.01), circle.x],
-      'y': [ circle.y, (circle.y - circle.jumpDistance/2), circle.y - circle.jumpDistance]
-    };
-
-    if (circle.jumpready === true) {
-      if (circle.lastCurve) {
-        circle.lastCurve.destroy();
-        circle.lastCurveMirror.destroy();
-      }
-      circle.pointGraphics.forEach(function (graphic) {
-        graphic.destroy();
-      });
-      circle.pointGraphics = [];
-      var jumpPath = game.add.graphics(circle.x + 2, circle.y + 2);
-      jumpPath.lineStyle(5, 0x99FFCC, 0.5);
-      jumpPath.quadraticCurveTo(
-        circle.jumpHeight - (circle.jumpHeight * 0.09), -circle.jumpDistance/2 - 5, 0 - 2, -circle.jumpDistance - 2
-      );
-      circle.lastCurve = jumpPath;
-
-      var jumpPathMirror = game.add.graphics(circle.x + 2, circle.y + 2);
-      jumpPathMirror.lineStyle(5, 0x99FFCC, 0.5);
-      jumpPathMirror.quadraticCurveTo(
-        -(circle.jumpHeight - (circle.jumpHeight * 0.09)), -circle.jumpDistance/2 - 5, 0 - 2, -circle.jumpDistance - 2
-      );
-      circle.lastCurveMirror = jumpPathMirror;
+    //add new platforms
+    //note: old platforms are not currently being destroyed
+    if ((circle.score % 10 === 0) && (addPlats === true)) {
+      console.log('add plats');
+      addPlatforms();
+      addPlats = false;
     }
+    else if (circle.score % 10 === 1) {
+      addPlats = true;
+    }
+  }
+
+  function render () {
+    //debug render info
+    // game.debug.cameraInfo(game.camera, 32, 32);
+    // game.debug.spriteCoords(circle, 32, game.height - 100);
   }
 
 };
---
+++ src/js/player.js
@@ -0,0 +1,193 @@
+var Player = function (game, playerSprite) {
+  //states
+  this.game = game;
+  this.jumping = false;
+  this.jumpready = false;
+
+  //score
+  this.score = 0;
+
+  //jump attrs
+  this.jumpDistance = 0;
+  this.jumpHeight = 0;
+  this.jumpHeightStart = 0;
+
+  //jump guide attrs
+  this.jumpGuideStartSpeed = 2.8;
+  this.jumpGuideSpeed = this.jumpGuideStartSpeed;
+  this.jumpGuideAccel = 1.03;
+  this.lastCurve = undefined; // to hold graphics object of last drawn curve - will be reset every frame
+
+  //jump path
+  this.HEIGHT_SENSITIVITY = 2;
+  this.POINT_DENSITY = 100;
+  this.pointGraphics = [];
+
+  //create sprite, set intitil event handler
+  Phaser.Sprite.call(this, game, game.width / 2, 0, playerSprite);
+  this.mirror = this.game.add.sprite(game.width / 2, 0, playerSprite);
+
+  this.anchor.set(0.5);
+  this.mirror.anchor.set(0.5);
+
+  this.game.input.onDown.add(this.prepareToJump, this);
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype, {
+
+  prepareToJump: {
+    value: function (clickEvent) {
+      // console.log('jumpprep');
+      // this.debugPrint();
+
+      this.jumpready = true;
+      this.jumpHeightStart = clickEvent.clientX;
+
+      this.game.input.onDown.remove(this.prepareToJump, this);
+      this.game.input.onUp.add(this.jump, this);
+    }
+  },
+
+  jump: {
+    value: function () {
+      // console.log('jump');
+
+      //speed of jump scales on both distance and height of jump - longer jumps
+      //are relatively slower, tiny jumps are relatively faster
+      var jumpSpeed = 10 * ((0.25 * this.jumpDistance) + (0.1 * this.jumpHeight));
+
+      //create tweens to animate the player & its mirror
+      var tween = this.jumpAnimate(this, jumpSpeed);
+      this.jumpAnimate(this.mirror, jumpSpeed);
+      tween.onComplete.add(this.land, this);
+
+      this.jumping = true;
+      this.jumpready = false;
+
+      this.jumpDistance = 0;
+      this.jumpHeight = 0;
+      this.jumpHeightStart = 0;
+      this.jumpGuideSpeed = this.jumpGuideStartSpeed;
+      this.jumpready = false;
+      this.game.input.onUp.remove(this.jump, this);
+    }
+  },
+
+  land: {
+    value: function () {
+      // console.log('landed');
+
+      this.jumping = false;
+      this.fadeTrail();
+
+      this.score += 1;
+      this.game.input.onDown.add(this.prepareToJump, this);
+    }
+  },
+
+  fadeTrail: {
+    value: function () {
+
+      //set tweens to fade out the old trail
+      oldCurve = this.lastCurve;
+      oldCurveMirror = this.lastCurveMirror;
+      this.lastCurve = undefined;
+      this.lastCurveMirror = undefined;
+
+      fade = this.game.add.tween(oldCurve);
+      fade.to({ alpha: 0 }, 250);
+      fade.onComplete.add(function () {
+        oldCurve.destroy();
+      });
+
+      fadeMirror = this.game.add.tween(oldCurveMirror);
+      fadeMirror.to({ alpha: 0 }, 250);
+      fadeMirror.onComplete.add(function () {
+        oldCurveMirror.destroy();
+      });
+
+      fade.start();
+      fadeMirror.start();
+    }
+  },
+
+  jumpAnimate: {
+    value: function (sprite, speed) {
+      var tween = this.game.add.tween(sprite);
+      tween.to({
+        x: sprite.points.x,
+        y: sprite.points.y
+      }, speed);
+      tween.interpolation(Phaser.Math.bezierInterpolation);
+      tween.start();
+
+      return tween;
+    }
+  },
+
+  debugPrint: {
+    value: function () {
+      console.log('jumping: ' + this.jumping);
+      console.log('jumpready ' + this.jumpready);
+
+      //jump attrs
+      console.log('jumpDist ' + this.jumpDistance);
+      console.log('jumpHeight ' + this.jumpHeight);
+      console.log('jumpStart ' + this.jumpHeightStart);
+
+      //jump guide attrs
+      console.log('jumpGuideSpeed ' + this.jumpGuideSpeed);
+    }
+  },
+});
+
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function () {
+
+  //update jumpHeight based on an accelerating guide curve
+  if (this.jumpready === true) {
+    this.jumpGuideSpeed *= this.jumpGuideAccel;
+    this.jumpDistance += this.jumpGuideSpeed;
+    this.jumpHeight = Math.abs(this.jumpHeightStart - this.game.input.x) * this.HEIGHT_SENSITIVITY;
+  }
+
+  //calculate jump path & mirror path
+  this.points = {
+    'x': [ this.x, this.x + this.jumpHeight + (this.jumpHeight * 0.01), this.x],
+    'y': [ this.y, (this.y - this.jumpDistance/2), this.y - this.jumpDistance]
+  };
+
+  this.mirror.points = {
+    'x': [ this.mirror.x, this.mirror.x - this.jumpHeight - (this.jumpHeight * 0.01), this.mirror.x],
+    'y': [ this.mirror.y, (this.mirror.y - this.jumpDistance/2), this.mirror.y - this.jumpDistance]
+  };
+
+  //redraw guide curve
+  if (this.jumpready === true) {
+    if (this.lastCurve) {
+      this.lastCurve.destroy();
+      this.lastCurveMirror.destroy();
+    }
+    this.pointGraphics.forEach(function (graphic) {
+      graphic.destroy();
+    });
+    this.pointGraphics = [];
+
+    //draw guide curve
+    var jumpPath = this.game.add.graphics(this.x + 2, this.y + 2);
+    jumpPath.lineStyle(5, 0x99FFCC, 0.5);
+    jumpPath.quadraticCurveTo(
+      this.jumpHeight - (this.jumpHeight * 0.09), -this.jumpDistance/2 - 5, 0 - 2, -this.jumpDistance - 2
+    );
+    this.lastCurve = jumpPath;
+
+    //draw guide curve mirror
+    var jumpPathMirror = this.game.add.graphics(this.x + 2, this.y + 2);
+    jumpPathMirror.lineStyle(5, 0x99FFCC, 0.5);
+    jumpPathMirror.quadraticCurveTo(
+      -(this.jumpHeight - (this.jumpHeight * 0.09)), -this.jumpDistance/2 - 5, 0 - 2, -this.jumpDistance - 2
+    );
+    this.lastCurveMirror = jumpPathMirror;
+  }
+};
