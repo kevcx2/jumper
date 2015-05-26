97c8fd56cb55b79415c8a4ef94cff8c0d1a3a4fb
kevcx2 <kev.r.cox90@gmail.com>
2015-05-25 21:56:32 -0700

    Update jumper.js

--- src/js/jumper.js
+++ src/js/jumper.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-  var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {
+  var game = new Phaser.Game(1080, 1900, Phaser.AUTO, '', {
     preload: preload,
     create: create,
     update: update,
